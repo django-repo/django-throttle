b7d8adf42da84bba77ba40af0a9a45c1be7bed2a
Ryan Febriansyah <37971350+sodrooome@users.noreply.github.com>
2020-10-20 12:06:38 +0700

    feat: separate class for simple rate throttle

---
+++ src/throttle/simple_throttle.py
@@ -0,0 +1,26 @@
+class SimpleRateThrottle(BaseThrottle):
+    cache = default_cache
+    timer = time.time
+    cache_format = 'throttle_%(scope)s_%(ident)s'
+    scope = None
+    THROTTLE_RATES = api_settings.DEFAULT_THROTTLE_RATES
+
+    def __init__(self):
+        if not getattr(self, 'rate', None):
+            self.rate = self.get_rate()
+        self.num_requests, self.duration = self.parse_rate(self.rate)
+
+    def get_cache_key(self, request, view):
+        raise NotImplementedError('.get_cache_key() must be overridden')
+
+    def get_rate(self):
+        if not getattr(self, 'scope', None):
+            msg = ("You must set either `.scope` or `.rate` for '%s' throttle" %
+                   self.__class__.__name__)
+            raise ImproperlyConfigured(msg)
+
+        try:
+            return self.THROTTLE_RATES[self.scope]
+        except KeyError:
+            msg = "No default throttle rate set for '%s' scope" % self.scope
+            raise ImproperlyConfigured(msg)
